e6d0d749efec7da7692e4d3648a0245b2e473936
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-26 17:20:36 +0100

    feat: implement article addition and deletion functionality in Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,25 @@
 import { useState } from 'react'
-import articles from '../data/articles'
+import myArticles from '../data/articles'
 
 export default function Main() {
-    const [article, setNewArticle] = useState('')
+    const [articles, setArticles] = useState(myArticles)
+    const [newArticle, setNewArticle] = useState('')
+    function handleAddArticle(e) {
+        e.preventDefault()
+        if (newArticle === '') {
+            alert('The field must not be empty')
+        }
+        setArticles([...articles, newArticle])
+        console.log(articles);
+        console.log(newArticle);
 
+
+        setNewArticle('')
+    }
+
+    function handleDeleteLast() {
+        setArticles(articles.slice(0, -1))
+    }
     return (
         <div className="container">
             <ul className='articlesList list-group'>
@@ -12,21 +28,24 @@ export default function Main() {
                     <li className='list-group-item' key={index}>{article}</li>
                 ))}
             </ul>
-
-            <div class="mb-3">
-                <label htmlFor="new_article" class="form-label"></label>
-                <input
-                    type="text"
-                    class="form-control p-2 mt-2"
-                    name="new_article"
-                    id="new_article"
-                    aria-describedby="helpId"
-                    placeholder="Insert a new article"
-                />
-                <small id="helpId" class="form-text text-muted"></small>
-            </div>
-            <button type='submit' className='btn btn-primary'>Submit</button>
-
+            <form onSubmit={handleAddArticle}>
+                <div className="mb-3">
+                    <label htmlFor="new_article" className="form-label"></label>
+                    <input
+                        type="text"
+                        className="form-control p-2 mt-2"
+                        name="new_article"
+                        id="new_article"
+                        aria-describedby="helpId"
+                        placeholder="Insert a new article"
+                        value={newArticle}
+                        onChange={(e) => setNewArticle(e.target.value)}
+                    />
+                    <small id="helpId" className="form-text text-muted"></small>
+                </div>
+                <button type='submit' className='btn btn-primary'>Submit</button>
+            </form>
+            <button type='button' className='btn btn-danger mt-3' onClick={handleDeleteLast}>Delete</button>
         </div>
 
     )
