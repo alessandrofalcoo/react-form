18310e4e81ff4ca87d5d69ffa5f6ea3f00d74701
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-26 16:12:20 +0100

    feat: add input field and submit button for new article entry in Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,15 +2,31 @@ import { useState } from 'react'
 import articles from '../data/articles'
 
 export default function Main() {
+    const [article, setNewArticle] = useState('')
 
     return (
         <div className="container">
-            <ul className='articlesList '>
+            <ul className='articlesList list-group'>
 
                 {articles.map((article, index) => (
-                    <li key={index}>{article}</li>
+                    <li className='list-group-item' key={index}>{article}</li>
                 ))}
             </ul>
+
+            <div class="mb-3">
+                <label htmlFor="new_article" class="form-label"></label>
+                <input
+                    type="text"
+                    class="form-control p-2 mt-2"
+                    name="new_article"
+                    id="new_article"
+                    aria-describedby="helpId"
+                    placeholder="Insert a new article"
+                />
+                <small id="helpId" class="form-text text-muted"></small>
+            </div>
+            <button type='submit' className='btn btn-primary'>Submit</button>
+
         </div>
 
     )
