ffba76eb9ca0a4b8bf75eb9aa3ce7dc8296985f5
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-26 15:46:32 +0100

    refactor: simplify articles data structure and update titles

--- src/data/articles.js
+++ src/data/articles.js
@@ -1,8 +1,2 @@
-const articles = [
-    { title: "Introduzione a React" },
-    { title: "Guida Completa a JavaScript" },
-    { title: "Come Utilizzare le API in React" },
-    { title: "Consigli per Migliorare le Prestazioni del Tuo Sito Web" }
-];
-
+const articles = ["React Intro", "JS Guide", "Using APIs", "Web Performance Tips"];
 export default articles;
